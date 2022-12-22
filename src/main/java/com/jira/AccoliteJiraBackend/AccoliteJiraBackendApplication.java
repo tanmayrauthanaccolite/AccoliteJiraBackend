75495442ee6ee73a02494daff3e8330af5d7f515
Tanmay Rauthan <tanmay.rauthan@accolite.com>
2022-12-22 11:45:52 +0530

    Added Spring Configurations

---
+++ src/main/java/com/jira/AccoliteJiraBackend/AccoliteJiraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jira.AccoliteJiraBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccoliteJiraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccoliteJiraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jira/AccoliteJiraBackend/AccoliteJiraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.AccoliteJiraBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccoliteJiraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
